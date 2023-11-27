495650c48deeb56e2e96bf87b66470aab61fd180
GovindPatil75 <mulegr75@gmail.com>
2023-11-27 09:10:49 +0530

    session 19

--- src/test/java/TestDemo/Session_18A.java
+++ src/test/java/TestDemo/Session_18A.java
@@ -1,7 +1,9 @@
 package TestDemo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,17 +13,34 @@ public class Session_18A {
 	public static void main(String[] args) throws Exception {
 		
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
         driver.get("https://www.facebook.com/");
         driver.manage().window().maximize();
         
-        JavascriptExecutor js=driver;
+       // JavascriptExecutor js=(JavascriptExecutor) driver;
+           //Scroll By Pixel
         //Top-bottom 
-        js.executeScript("window.scrollBy(0,500)", "");
+       // js.executeScript("window.scrollBy(0,500)", "");
         
-        Thread.sleep(3000);
+       // Thread.sleep(3000);
         //Bottom-top
-        js.executeScript("window.scrollBy(0,-500)","");
+        //js.executeScript("window.scrollBy(0,-500)","");
+        
+        WebElement element=driver.findElement(By.xpath("//li[text()='English (UK)']"));
+        
+        //js.executeScript("arguments[0].scrollIntoView();", element);
+        
+        
+        ScrollByElement(driver,element);
 	}
 
+	public static void ScrollByElement(WebDriver driver,WebElement element) {
+		
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		
+		js.executeScript("arguments[0].scrollIntoView();", element);
+		
+		
+	}
+	
 }
