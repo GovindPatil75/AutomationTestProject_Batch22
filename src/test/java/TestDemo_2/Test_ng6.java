ad24f423f4163965ab2cac1913a1211d0efe204c
GovindPatil75 <mulegr75@gmail.com>
2023-12-09 09:20:41 +0530

    Session TestNG 5

---
+++ src/test/java/TestDemo_2/Test_ng6.java
@@ -0,0 +1,24 @@
+package TestDemo_2;
+
+import org.testng.annotations.Test;
+
+public class Test_ng6 extends BaseClass{
+	
+	
+	@Test
+	public void t1() {
+		System.out.println("Test T1");
+	}
+	
+	@Test
+	public void t2() {
+		System.out.println("Test T2");
+	}
+	
+	@Test
+	public void t3() {
+		System.out.println("Test T3");
+	}
+	
+
+}
